feef93e6cddf740e4f1deec0243b23cb3bdf4298
Ranjitha1304 <ranjitha13cs@gmail.com>
2025-01-17 18:52:23 +0530

    Delete JS directory

--- JS/popular.js
+++
@@ -1,9 +0,0 @@
-function toggleSection() {
-    const content = document.getElementById("expandable-content");
-
-    if (content.style.display === "none" || content.style.display === "") {
-      content.style.display = "block"; // Show content
-    } else {
-      content.style.display = "none"; // Hide content
-    }
-  }
